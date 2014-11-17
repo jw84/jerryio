bb15468306bae94086fa6629e747f35180e4fc19
jw84 <jerrywang@me.com>
2014-11-16 17:44:21 -0800

    hide scratch

--- 0scratch/app.js
+++
@@ -1,8 +0,0 @@
-var connect = require('connect')
-var serveStatic = require('serve-static')
-
-var app = connect()
-
-app.use(serveStatic('particles.js/demo', {'index': ['index.html']}))
-console.log('Server 3k')
-app.listen(3000)
